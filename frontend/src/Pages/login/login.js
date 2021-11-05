474685afc4b7467e0415594d91328c76de319f68
Ajay Bharat Kolla <ajaybharat222@gmail.com>
2021-11-05 19:21:01 +0530

    removed unused eseeffect

--- frontend/src/Pages/login/login.js
+++ frontend/src/Pages/login/login.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import "./login.css";
 import { connect } from 'react-redux'
