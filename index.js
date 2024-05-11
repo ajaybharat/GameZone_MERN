ff03903b2e3503ecd68e90831cf361e360607db3
Ajay Kolla <ajkolla@appdynamics.com>
2024-05-11 23:52:03 +0530

    update cors

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ require("dotenv").config();
 
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+    exposedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json()); // like middleware in pipeline
 app.use('/api/auth',authRoutes);
 app.use('/api/user',userRoutes);
